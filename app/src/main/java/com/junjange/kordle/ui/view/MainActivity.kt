88d2d44882a37c2dd7b76c66bea70bc9656d6977
junjange <wnswkd486@naver.com>
2022-06-29 01:16:00 +0900

    22.06.29Update

--- app/src/main/java/com/junjange/kordle/ui/view/MainActivity.kt
+++ app/src/main/java/com/junjange/kordle/ui/view/MainActivity.kt
@@ -2,8 +2,11 @@ package com.junjange.kordle.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.view.children
 import androidx.lifecycle.ViewModelProvider
 import com.junjange.kordle.R
 import com.junjange.kordle.databinding.ActivityMainBinding
@@ -18,11 +21,74 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
-        binding.qButton.setOnClickListener {
-            Toast.makeText(this@MainActivity,"kooooooooorgle", Toast.LENGTH_SHORT).show()
+
+        val boxText = arrayOf(arrayOf(binding.box0Text,  binding.box1Text,  binding.box2Text,  binding.box3Text,  binding.box4Text,  binding.box5Text),
+            arrayOf(binding.box6Text,  binding.box7Text,  binding.box8Text,  binding.box9Text,  binding.box10Text,  binding.box11Text),
+            arrayOf(binding.box12Text,  binding.box13Text,  binding.box14Text,  binding.box15Text,  binding.box16Text,  binding.box17Text),
+            arrayOf(binding.box18Text,  binding.box19Text,  binding.box20Text,  binding.box21Text,  binding.box22Text,  binding.box23Text),
+            arrayOf(binding.box24Text,  binding.box25Text,  binding.box26Text,  binding.box27Text,  binding.box28Text,  binding.box29Text),
+            arrayOf(binding.box30Text,  binding.box31Text,  binding.box32Text,  binding.box33Text,  binding.box34Text,  binding.box35Text))
+
+
+
+        val line = Array(6){ i -> false}
+        var currentLine : Int = 0
+        var currentBox : Int = 0
+
+        viewModel.keyboardText.observe(this, {
+
+            viewModel.keyboardText.value?.let { it ->
+
+                if(!line[currentLine] && currentBox < 6){
+                    boxText[currentLine][currentBox].text = it
+
+                    currentBox++
+
+                }else{
+                    Toast.makeText(this@MainActivity,"6개의 자음과 모음으로 단어를 확인하세요!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        })
+
+//        binding.qButton.setOnClickListener {
+//            Log.d("ttt", currentLine.toString())
+//            if(!line[currentLine] && currentBox < 6){
+//                boxText[currentLine][currentBox].text = "ㅂ"
+//                currentBox++
+//
+//            }else{
+//                Toast.makeText(this@MainActivity,"6개의 자음과 모음으로 단어를 확인하세요!", Toast.LENGTH_SHORT).show()
+//
+//            }
+//        }
+
+        binding.deleteButton.setOnClickListener{
+            if(currentBox in 1..6){
+                boxText[currentLine][currentBox-1].text = ""
+                currentBox--
+
+            }
+
+        }
+
+        binding.submitButton.setOnClickListener {
+            if(currentBox < 6){
+                Toast.makeText(this@MainActivity,"6개의 자음과 모음으로 단어를 입력하세요!", Toast.LENGTH_SHORT).show()
+
+
+            }else{
+                line[currentLine] = true
+                currentLine++
+                currentBox = 0
+            }
+
         }
 
 
 
     }
+
+
 }
--- app/src/main/java/com/junjange/kordle/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/junjange/kordle/ui/viewmodel/MainViewModel.kt
@@ -1,12 +1,30 @@
 package com.junjange.kordle.ui.viewmodel
 
 import android.app.Application
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val _keyboardText = MutableLiveData<String>()
+
+    val keyboardText : MutableLiveData<String>
+    get() = _keyboardText
+
+    private val keyboard = hashMapOf(
+        "q_button" to "ㅂ", "w_button" to "ㅈ", "e_button" to "ㄷ", "r_button" to "ㄱ", "t_button" to "ㅅ", "y_button" to "ㅛ", "u_button" to "ㅕ", "i_button" to "ㅑ",
+        "a_button" to "ㅁ", "s_button" to "ㄴ", "d_button" to "ㅇ", "f_button" to "ㄹ", "g_button" to "ㅎ", "h_button" to "ㅗ", "j_button" to "ㅓ", "k_button" to "ㅏ", "l_button" to "ㅣ",
+        "z_button" to "ㅋ", "x_button" to "ㅌ", "c_button" to "ㅊ", "v_button" to "ㅍ", "b_button" to "ㅠ", "n_button" to "ㅜ", "m_button" to "ㅡ"
+    )
+
+    fun keyboardOnClick(view: View){
+
+        _keyboardText.value = keyboard[view.resources.getResourceEntryName(view.id)]
 
 
+    }
 }
